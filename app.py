da821cb0c2932b7ebb57b24c0f89d21273d6d4d4
najm <najm.gmail.com>
2025-01-12 15:13:43 +0300

    djvhvkj

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ async def get_captions(vd: str):
     print(vd)
     try:
         # استخراج النصوص من الفيديو
-        transcript = YouTubeTranscriptApi.list_transcripts(vd).find_generated_transcript(["ar"]).fetch()
+        transcript = YouTubeTranscriptApi.list_transcripts(vd).find_generated_transcript(["ar", "en"]).fetch()
         return {"status": "success", "captions": transcript}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))

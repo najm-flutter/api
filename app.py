73990697ab0473458cb7f4a04dd49e9c5376586d
najm <najm.gmail.com>
2025-01-12 14:48:00 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, HTTPException
+from youtube_transcript_api import YouTubeTranscriptApi
+
+app = FastAPI()
+
+# نموذج الإدخال
+
+@app.get("/get-captions/")
+async def get_captions(vd: str):
+    print(vd)
+    try:
+        # استخراج النصوص من الفيديو
+        transcript = YouTubeTranscriptApi.list_transcripts(vd).find_generated_transcript(["ar"]).fetch()
+        return {"status": "success", "captions": transcript}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
